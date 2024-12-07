b097adfd0708db1dfa6c0a310a063ec4f170040a
meryymarta <meriammartadinata@gmail.com>
2024-12-08 05:37:25 +0700

    2.1 experiment 1

---
+++ RecursiveStudent.java
@@ -0,0 +1,26 @@
+public class RecursiveStudent {
+    static int factorialRecursive(int n) {
+        if(n==1)
+            return 1;
+        else
+            return n*factorialRecursive(n-1);
+    }
+
+            static int factorialLiterative(int n){
+                int factorial = 1;
+                for(int i = n; i >= 1; i--){
+                    factorial = factorial*i;
+                }
+                return factorial;
+            }
+
+        public static void main(String[] args) {
+            System.out.println("Hello world!!!");
+            System.out.println(factorialRecursive(5));
+            System.out.println(factorialLiterative(5));
+        }
+    }
+         
+    
+    
+
